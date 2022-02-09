0533401262c43b516f364d91f90985d983947957
190763 <190763@E0505-19>
2022-02-09 09:29:52 +0900

    getPointの完成（仮）

--- src/GetPointServlet.java
+++ src/GetPointServlet.java
@@ -1,6 +1,6 @@
 
-
 import java.io.IOException;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.DAO;
 /**
  * Servlet implementation class GetPointServlet
  */
@@ -28,8 +29,34 @@ public class GetPointServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
-		rd.forward(request, response);
+		//--- 入出力用文字エンコード
+		response.setCharacterEncoding("utf-8");
+		request.setCharacterEncoding("utf-8");
+		//ポイント格納用変数
+		int point;
+		
+		try {
+			//--- パラメータ（TENPO_IDとUSER_ID）の取得
+			String tenpoId = request.getParameter("TENPO_ID");
+			String userId = request.getParameter("USER_ID");
+			
+			//--- DAOオブジェクトのインスタンス化
+			DAO dao = new DAO();
+			//--- DAOのsearchAndInsertメソッドを使ってデータを取得
+			point = dao.searchAndInsert(tenpoId, userId);
+			
+			//--- 表示用のJSPへ転送
+			//--- 転送するデータにpointという名前を付けて設定
+			request.setAttribute("point", point);
+			//--- getPoint.jsp へ転送
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/getPoint.jsp");
+			rd.forward(request, response);
+		} catch (Exception e) {
+			//--- 改行コードや空白文字を有効にするタグを発行
+			response.getWriter().println("<pre>");
+			response.getWriter().println(e.getMessage());
+			e.printStackTrace();
+		}	
 	}
 
 }
